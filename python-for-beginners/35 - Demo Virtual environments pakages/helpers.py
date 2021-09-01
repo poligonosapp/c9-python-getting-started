d4cc4cf4979abe203e5f4b022fcaa7cce80afa7b
Luis Mendes <machadoluismendes@gmail.com>
2021-09-01 11:51:32 -0300

    Fore

--- python-for-beginners/35 - Demo Virtual environments pakages/helpers.py
+++ python-for-beginners/35 - Demo Virtual environments pakages/helpers.py
@@ -3,5 +3,5 @@ from pip._vendor.colorama import init, Fore
 def display(message, is_warning=False):
     if is_warning:
         print('This is a warning')
-    print(FORE.BLUE + message)
+    print(Fore.BLUE + message)
     
